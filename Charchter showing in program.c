26a2528860ccdd6c143a9810b8778e0fd0d3be40
zakirdesigner0708 <zakir.designer0708@gmail.com>
2024-09-02 00:58:52 +0530

    [descrip]

---
+++ Charchter showing in program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  float myNum = 5.22;  // double
+  char ch='c';
+  int i=10;
+  
+  //printf("%f\n", myNum);
+  //printf("%c\n", ch);
+  //printf("%d\n", i);
+char a = 1, b = 2, x = '65', y = '66', z = '67', c =3; // it will show char only because digit in not in '' so A , B or c it will show
+printf("%c\n", a);
+printf("%c\n", b);
+printf("%c\n", c);
+printf("%c\n", x);
+printf("%c\n", y);
+printf("%c\n", z);
+  return 0;
+}
