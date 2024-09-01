c956cf5afac1a93eaee98c720f987802d77434a7
zakirdesigner0708 <zakir.designer0708@gmail.com>
2024-09-02 00:30:39 +0530

    [New File Added]

---
+++ Array_Example.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int array[] = {7, 12, 9, 4, 11};  // Corrected array declaration
+    
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+
+    printf("%d\n", array[0]);         // Corrected printf statement
+    return 0;
+}
--- Lo.c
+++ Lo.c
@@ -1,6 +1,12 @@
-#include<stdio.h>
-int main(){
-    int i=15;
-    printf("Hello B");
-    return 0;
+#include <stdio.h>
+
+int main() {
+  float myNum = 5.22;  // double
+  char ch='c';
+  int i=10;
+  
+  printf("%f\n", myNum);
+  printf("%c\n", ch);
+  printf("%d\n", i);
+  return 0;
 }
