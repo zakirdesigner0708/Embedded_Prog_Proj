2a6cbd7b1c8ef1be947029753d8cb76db1fbd39e
zakirdesigner0708 <zakir.designer0708@gmail.com>
2024-09-02 08:02:42 +0530

    [Const_Program_added_in_2_dec_float]

---
+++ Constant_exam.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  const int myNum = 15;
+  //myNum = 10; //this will show error when you will remove the comment from program
+  printf("This is my number\n\n\n");
+  printf("%d", myNum);
+  return 0;
+}
---
+++ Constant_example_with_2Dec_float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+const float numpy=5.999;
+//numpi=10;
+  printf("%f\n", numpy);
+printf("%.1f\n", numpy);
+printf("%.2f\n", numpy);
+printf("%.3f\n", numpy);
+printf("%.4f\n", numpy);
+  return 0;
+}
